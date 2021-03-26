e47454b29f137e61bec260a9c1c4de44fd0465c4
davidmandia <david.mandia@yahoo.com>
2021-03-26 21:57:07 +0000

    simple component

---
+++ client/src/Book.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './book.css';
+
+
+const Book = () => {
+    return (
+        <div id='book-message'>
+            I Am a book
+            
+        </div>
+    )
+}
+
+export default Book;
